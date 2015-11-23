ceac409d06e9882e7f1a646ecb63c55104d83222
Ryan Thompson <ryan@pyrocms.com>
2015-11-23 08:48:00 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# html_widget-extension
---
+++ resources/lang/en/addon.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'title'       => 'Html Widget-extension',
+    'name'        => 'Html Widget-extension Extension',
+    'description' => ''
+];
